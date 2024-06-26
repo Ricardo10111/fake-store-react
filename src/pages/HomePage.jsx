6d8c0a0e3e2672d55ee0b616c08368be8af0b36e
Ricardo Lopez <albelop1m@gmail.com>
2024-06-26 00:32:11 -0600

    chore: changes in homePaage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,31 @@
-import { Link } from "react-router-dom"
+import { Link } from 'react-router-dom'
 
 export default function HomePage() {
-    return (
-        <div>
-            <h1 className="text-center text-black font-bold text-[30px]">Welcome to the Home Page</h1>
-            <p>This is the Home Page content</p>
-            <div className="flex flex-col justify-center items-center gap-5">
-                <Link to="/login" className="bg-blue-300 border-blue-600">Login</Link>
-                <Link to="/products">Products</Link>
-                <Link to="/products/123">Producto 123</Link>
-            </div>
-        </div>
-    )
+  return (
+    <main className='flex justify-center items-center flex-col gap-4  w-full min-h-dvh'>
+      <h1 className='text-4xl font-semibold text-center'>
+        Welcome to the Home Page
+      </h1>
+      <div className=' rounded p-4 flex flex-col gap-4 max-w-sm w-full'>
+        <Link
+          to='/login'
+          className='bg-blue-500 text-white border-2 border-blue-600 rounded-lg p-2 text-center'
+        >
+          Login
+        </Link>
+        <Link
+          to='/products'
+          className='bg-blue-500 text-white border-2 border-blue-600 rounded-lg p-2 text-center'
+        >
+          Products
+        </Link>
+        <Link
+          to='/products/123'
+          className='bg-blue-500 text-white border-2 border-blue-600 rounded-lg p-2 text-center'
+        >
+          Producto 123
+        </Link>
+      </div>
+    </main>
+  )
 }
